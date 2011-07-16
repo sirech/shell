62a48ca39c317312754b0333572e17104d87aa01
Mario Fernandez <mario.fernandez@hceris.com>
2011-07-16 19:48:26 +0200

    os specific config

---
+++ zshrc/os/linux.zsh
@@ -0,0 +1,17 @@
+# -*- mode: Shell-script-*-
+
+#
+# Linux specific settings
+#
+
+## DEFUNS
+
+## ENV
+
+## ALIASES
+
+# redefine ls
+alias ls='ls -lh --color="auto"'
+
+# use a shorter name for ack-grep
+alias ack='ack-grep'
---
+++ zshrc/os/macosx.zsh
@@ -0,0 +1,36 @@
+# -*- mode: Shell-script-*-
+
+#
+# Mac OS X specific settings
+#
+
+## DEFUNS
+
+# Changes the profile of the current Terminal window to the given one.
+function change_profile() {
+        /usr/bin/osascript <<EOF
+tell application "Terminal" to set current settings of first window to settings set named "${1:?No profile name provided}"
+EOF
+}
+
+
+## ENV
+
+export JAVA_HOME="/Library/Java/Home"
+export ANT_HOME="/usr/share/ant"
+
+# Use colors than can actually be seen
+export CLICOLOR=1
+export LSCOLORS=DxGxcxdxCxegedabagacad
+
+
+## ALIASES
+
+# cleanup DS_Store files in the current directory
+alias dsclean='find . -name ".DS_Store" | xargs rm'
+
+# MacPorts
+alias port='sudo port'
+
+# redefine ls
+alias ls='ls -lhG'
