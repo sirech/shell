141984ef5f4584c85a6c6320a77d79dc6ace270a
Mario Fernandez <mario.fernandez@hceris.com>
2011-07-17 14:16:17 +0200

    machine specific config

---
+++ zshrc/machines/Killua.zsh
@@ -0,0 +1,17 @@
+# -*- mode: Shell-script-*-
+
+#
+# Settings for the machine 'Killua'
+#
+
+## ENV
+
+export PATH="${PATH}:${C}/Python27:${C}/Python27/scripts"
+export WINHOME="${C}/Users/Mario/AppData/Roaming"
+export EMACS_DIR="${WINHOME}/.emacs.d"
+
+export WORKSPACE="${C}/workspace"
+
+## ALIASES
+
+alias gospace="cd ${WORKSPACE}"
