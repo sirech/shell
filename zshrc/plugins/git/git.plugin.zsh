19067f319d769b6b00f6054f6df07634ef30f5ff
Mario Fernandez <mario.fernandez@hceris.com>
2011-07-31 20:51:14 +0200

    removed useless stuff for aliases

--- zshrc/plugins/git/git.plugin.zsh
+++ zshrc/plugins/git/git.plugin.zsh
@@ -86,15 +86,9 @@ function add_to_github () {
 ## ALIASES
 
 alias gs='git status'
-compdef _git gs=git-status
 alias gpa='git push -v && git push -v public master'
-compdef _git gpa=git-push
 alias gu='git pull -v'
-compdef _git gu=git-pull
 alias gup='git pull -v && git push -v'
 alias ga='git add -v'
-compdef _git ga=git-add
 alias gd='git diff'
-compdef _git gd=git-diff
 alias gl='git log'
-compdef _git gl=git-log
