da468684e94153deba185004fe6fa1a01bdebf42
Mario Fernandez <mario.fernandez@friendscout24.de>
2012-10-02 16:06:47 +0200

    Don't crash if a rails project does not use ActiveRecord

--- irbrc/rails.rb
+++ irbrc/rails.rb
@@ -18,7 +18,11 @@ if rails_env = ENV['RAILS_ENV']
   # Called after the irb session is initialized and Rails has
   # been loaded (props: Mike Clark).
   IRB.conf[:IRB_RC] = Proc.new do
-    ActiveRecord::Base.logger = Logger.new(STDOUT)
-    ActiveRecord::Base.instance_eval { alias :[] :find }
+    begin
+      ActiveRecord::Base.logger = Logger.new(STDOUT)
+      ActiveRecord::Base.instance_eval { alias :[] :find }
+    rescue NameError
+      # ActiveRecord is not used
+    end
   end
 end
