765fac3696c4691afe34bf9dfa182f9726527d94
Mario Fernandez <mario.fernandez@friendscout24.de>
2012-11-26 17:45:07 +0100

    Save more history for irb

--- irbrc/history.rb
+++ irbrc/history.rb
@@ -1,5 +1,5 @@
 # Save History between irb sessions
 desire('irb/ext/save-history') do
-  IRB.conf[:SAVE_HISTORY] = 100
+  IRB.conf[:SAVE_HISTORY] = 2500
   IRB.conf[:HISTORY_FILE] = "#{ENV['HOME']}/.irb-save-history"
 end
