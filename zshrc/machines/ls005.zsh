727de738f8ebb4a8a53eca9950e9e49c01a4e5c8
Mario Fernandez <mario.fernandez@xing.com>
2014-07-17 14:33:32 +0200

    Add function to execute stuff on remote servers

--- zshrc/machines/ls005.zsh
+++ zshrc/machines/ls005.zsh
@@ -10,3 +10,44 @@ export CUCUMBER_HOST=stable-mario-fernandez.env.xing.com
 export JRE_HOME=/opt/jre
 
 path=($JRE_HOME/bin $path)
+
+function app_servers_old {
+    echo events-1.app.fra{1,2}.xing.com
+}
+
+function api_servers_old {
+    echo events-{1,2}.api.fra{1,2}.xing.com
+}
+
+function worker_servers_old {
+    echo events-1.worker.fra{1,2}.xing.com
+}
+
+function app_servers {
+    echo events-{2,3}.app.fra{1,2}.xing.com
+}
+
+function api_servers {
+    echo events-{3,4}.api.fra{1,2}.xing.com
+}
+
+function worker_servers {
+    echo events-{2,3}.worker.fra{1,2}.xing.com
+}
+
+function multirun {
+    host=${1:?No hosts given}
+    cmd=${2:?No cmd given}
+
+    case $host in
+        app )
+            boxes=$(app_servers_old) ;;
+        api )
+            boxes=$(api_servers_old) ;;
+        worker )
+            boxes=$(worker_servers_old) ;;
+    esac
+
+    mussh -h $boxes -c $cmd
+}
+
