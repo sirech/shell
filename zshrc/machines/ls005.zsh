4e9bdf2c1fc99c05c9dae6795e98f191580f2ac7
Mario Fernandez <mario.fernandez@xing.com>
2017-01-03 11:41:14 +0100

    Add ESPFP key

--- zshrc/machines/ls005.zsh
+++ zshrc/machines/ls005.zsh
@@ -9,6 +9,7 @@
 # Access JIRA from the console
 export JIRA_URL=https://jira.xing.hh/jira
 export HERMES_SANDBOX=http://hood-mario-fernandez.env.xing.com/
+export ESPFP_API_KEY=c0abac272b72f7037f8367ef9251cd86
 
 ## ALIASES
 
