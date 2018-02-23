e9cbfeec14ea97bda93df4ad16dc5dca20b6d039
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-02-23 09:17:09 +0100

    remove vars

--- zshrc/machines/hisoka.zsh
+++ zshrc/machines/hisoka.zsh
@@ -5,9 +5,6 @@
 #
 
 ## ENV
-export VAULT_ADDR=https://mcmea.es.corpintra.net:8200
-export VAULT_SKIP_VERIFY=true
-export CONSUL_HTTP_ADDR=https://mcmea.es.corpintra.net:8500
 
 # Access JIRA from the console
 
