10c29e205a830b70b4393fcedc195b1fb1490720
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-05-27 17:42:27 +0200

    add cloud helpers for hisoka

--- zshrc/machines/hisoka.zsh
+++ zshrc/machines/hisoka.zsh
@@ -11,3 +11,8 @@ export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 
 ## ALIASES
 alias tf=terraform
+
+## Setup
+script_dir=~/workspace/cloud-helpers
+source ${script_dir}/cloud-helpers.sh
+# source ${script_dir}/cloud-helpers-completion.sh
