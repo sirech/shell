636007f625b4b73f12ae919021510482c924b345
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-03-14 22:56:38 +0100

    Add fzf plugin

--- zshrc/config/02_aliases.zsh
+++ zshrc/config/02_aliases.zsh
@@ -41,6 +41,7 @@ alias -g L="| less"
 alias -g N="| /dev/null"
 alias -g S='| sort'
 alias -g G='| grep' # now you can do: ls foo G something
+alias -g F='| fzf'
 
 # Ag does not have a proper configuration file
 alias ag='ag --pager "less -r" -C 1'
