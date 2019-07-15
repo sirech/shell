0ce2e05ec914c088cb78847bc0263a593f37c155
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-07-15 20:39:57 +0200

    add nvm plugin to auto switch version with nvm

---
+++ zshrc/plugins/nvm/nvm.plugin.zsh
@@ -0,0 +1,16 @@
+load_old_plugin $0
+
+## ENVIRONMENT
+
+autoload -U add-zsh-hook
+
+load-nvmrc() {
+  if [[ -f .nvmrc && -r .nvmrc ]]; then
+    nvm use
+  elif [[ $(nvm version) != $(nvm version default)  ]]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+
+add-zsh-hook chpwd load-nvmrc
