1ed9277028b5c2868ee049d790f2b59338a86bac
Mario Fernandez <mario.fernandez@hceris.com>
2020-02-25 16:52:41 +0100

    switch to n as package manager for node

--- zshrc/machines/hisoka.zsh
+++ zshrc/machines/hisoka.zsh
@@ -12,3 +12,8 @@
 alias tf=terraform
 
 ## Setup
+export N_PREFIX=$HOME/.n
+[ -d $N_PREFIX ] && export PATH=$N_PREFIX/bin:$PATH
+
+DAYTONA_INIT=~/work/dev-tools/etc/shell-init.sh
+[ -f $DAYTONA_INIT ] && . $DAYTONA_INIT
