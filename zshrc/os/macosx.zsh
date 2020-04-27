41ea9754ef28698b3f8749dba99a3e7ab4c1e14b
Mario Fernandez <mario.fernandez@hceris.com>
2020-04-27 11:48:56 +0200

    add openssl to lib path if present

--- zshrc/os/macosx.zsh
+++ zshrc/os/macosx.zsh
@@ -22,6 +22,8 @@ if test $(which brew); then
   [ -d /usr/local/opt/findutils ] && export PATH=/usr/local/opt/findutils/libexec/gnubin:$PATH
   [ -d /usr/local/opt/grep ] && export PATH=/usr/local/opt/grep/libexec/gnubin:$PATH
   [ -d /usr/local/opt/gnu-sed ] && export PATH=/usr/local/opt/gnu-sed/libexec/gnubin:$PATH
+
+  [ -d /usr/local/opt/openssl/lib ] && export LIBRARY_PATH=/usr/local/opt/openssl/lib:$LIBRARY_PATH
 fi
 
 ## ALIASES
