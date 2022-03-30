2db160c6a2a371e4457572cb379f426ddc032c7d
Mario Fernandez <mario.fernandez@hceris.com>
2022-03-30 13:07:50 +0200

    add kubectl plugin to support krew

---
+++ zshrc/plugins/kubectl/kubectl.plugin.zsh
@@ -0,0 +1,11 @@
+load_old_plugin $0
+
+## ENVIRONMENT
+
+# Add krew in case it's present
+export KREW_PREFIX=$HOME/.krew
+[ -d $KREW_PREFIX ] && export PATH=$KREW_PREFIX/bin:$PATH
+
+## FUNCTIONS
+
+## ALIASES
